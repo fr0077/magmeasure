33e8bd73776a2b211ef51c151d826ce4affdbfc6
ryohei <fr00777@hotmail.co.jp>
2019-04-10 17:03:57 +0900

    機器制御のヘッダを作成

---
+++ device/probe/probe.hpp
@@ -0,0 +1,29 @@
+#ifndef PROBE
+#define PROBE
+
+class Probe
+{
+  private:
+  public:
+    //ホールプローブを初期化する
+    //戻り値：成功なら0、失敗ならそれ以外
+    //すでに初期化済みであってもこの関数を呼び出しても問題ないようにする
+    int initialize();
+
+    //ホールプローブをゼロリセットする
+    //戻り値：成功なら0、失敗ならそれ以外
+    int setZero();
+
+    //ホールプローブの値
+    typedef struct {
+      //成功なら0、失敗ならそれ以外
+      int status;
+      //3軸の値
+      double x,y,z;
+    }Value;
+
+    //プローブの値を出力する
+    Value getValue();
+};
+
+#endif
